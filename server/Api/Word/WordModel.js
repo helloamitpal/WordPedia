9d87f69bbd95033a5cfec33885deb7d73635c4a8
IN22913318 <amit.pal@tesco.com>
2019-07-25 14:19:10 +0530

    updated readme doc

--- server/Api/Word/WordModel.js
+++ server/Api/Word/WordModel.js
@@ -1,10 +1,11 @@
 class WordModel {
-  constructor({ word, phonetic, origins, longDefinitions, shortDefinitions }) {
+  constructor({ word, phonetic, origins, longDefinitions, shortDefinitions, synonyms }) {
     this.word = word || '';
     this.phonetic = phonetic || '';
     this.origins = origins || [];
     this.longDefinitions = longDefinitions || [];
     this.shortDefinitions = shortDefinitions || [];
+    this.synonyms = synonyms || [];
   }
 }
 

7b228401f7bdded40b9a6e65177280153045dff3
IN22913318 <amit.pal@tesco.com>
2019-08-01 07:12:13 +0530

    Added common error handler

--- server/Api/Word/WordController.js
+++ server/Api/Word/WordController.js
@@ -1,16 +1,19 @@
 const logger = require('../../util/logger');
 const WordService = require('./WordService');
+const ErrorHandler = require('../../util/errorHandler');
 
 class WordController {
   getAllWords(req, res) {
     logger.info('WordController | getAllWords');
-    WordService.getAllWords().then((data) => {
-      logger.success(`WordController | getAllWords | sending success response | result count: ${data.length}`);
-      res.send(data);
-    }, () => {
-      logger.error('WordController | getAllWords | Sending error response');
-      res.status(500).send('Failed to get all words');
-    });
+    WordService.getAllWords()
+      .then((data) => {
+        logger.success(`WordController | getAllWords | sending success response | result count: ${data.length}`);
+        res.send(data);
+      })
+      .catch(() => {
+        logger.error('WordController | getAllWords | Sending error response');
+        ErrorHandler(500, 'Failed to get all words', res);
+      });
   }
 
   searchWord(req, res) {
@@ -18,13 +21,15 @@ class WordController {
     logger.info(`WordController | searchWord | Search text: "${searchText}"`);
 
     if (searchText.trim()) {
-      WordService.searchWord(searchText, searchType).then((data) => {
-        logger.success(`WordController | searchWord | Sending success response | Search text: "${searchText}"`);
-        res.send(data);
-      }, () => {
-        logger.error(`WordController | searchWord | Sending error response | Search text: "${searchText}"`);
-        res.status(500).send('Failed to search given word');
-      });
+      WordService.searchWord(searchText, searchType)
+        .then((data) => {
+          logger.success(`WordController | searchWord | Sending success response | Search text: "${searchText}"`);
+          res.send(data);
+        })
+        .catch(() => {
+          logger.error(`WordController | searchWord | Sending error response | Search text: "${searchText}"`);
+          ErrorHandler(500, 'Failed to search given word', res);
+        });
     } else {
       this.getAllWords(req, res);
     }
@@ -35,16 +40,18 @@ class WordController {
 
     if (payload) {
       logger.info(`WordController | addWord | word: "${payload.word}"`);
-      WordService.addWord(req.body).then((data) => {
-        logger.success(`WordController | addWord | Sending success response | word: "${payload.word}"`);
-        res.send(data);
-      }, () => {
-        logger.error(`WordController | addWord | Sending error response | word: "${payload.word}"`);
-        res.status(500).send('Failed to add word');
-      });
+      WordService.addWord(req.body)
+        .then((data) => {
+          logger.success(`WordController | addWord | Sending success response | word: "${payload.word}"`);
+          res.send(data);
+        })
+        .catch(() => {
+          logger.error(`WordController | addWord | Sending error response | word: "${payload.word}"`);
+          ErrorHandler(500, 'Failed to add word', res);
+        });
     } else {
       logger.error('WordController | addWord | Sending error response because body payload is null');
-      res.status(500).send('Failed to add word');
+      ErrorHandler(500, 'Failed to add word', res);
     }
   }
 
@@ -53,16 +60,18 @@ class WordController {
     const { word } = req.params;
 
     if (word) {
-      WordService.deleteWord(word).then((data) => {
-        logger.success(`WordController | deleteWord | Sending success response | word: "${word}"`);
-        res.send(data);
-      }, () => {
-        logger.error(`WordController | deleteWord | Sending error response | word: "${word}"`);
-        res.status(500).send('Failed to delete word');
-      });
+      WordService.deleteWord(word)
+        .then((data) => {
+          logger.success(`WordController | deleteWord | Sending success response | word: "${word}"`);
+          res.send(data);
+        })
+        .catch(() => {
+          logger.error(`WordController | deleteWord | Sending error response | word: "${word}"`);
+          ErrorHandler(500, 'Failed to delete word', res);
+        });
     } else {
       logger.error('WordController | addWord | Sending error response because invalid word found');
-      res.status(500).send('Failed to delete word');
+      ErrorHandler(500, 'Failed to delete word', res);
     }
   }
 }
--- server/Api/Word/WordModel.js
+++ server/Api/Word/WordModel.js
@@ -8,7 +8,8 @@ const WordModelSchema = new Schema({
   origins: { type: Array, default: [] },
   shortDefinitions: { type: Array, default: [] },
   longDefinitions: { type: Array, default: [] },
-  synonyms: { type: Array, default: [] }
+  synonyms: { type: Array, default: [] },
+  createdDate: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('WordModel', WordModelSchema);
